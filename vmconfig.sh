46547893b7d36e51fcb079d0078984269b4b0b38
Toso <tcorax4@gmail.com>
2023-06-23 17:33:19 +0200

    flat

--- vmconfig.sh
+++ vmconfig.sh
@@ -5,5 +5,7 @@
 # sudo apt install git
 # git clone git@github.com:Toso42/inception.git
 
-sudo apt update && sudo apt upgrade -y \
-sudo apt install vim
+sudo apt update && sudo apt upgrade -y
+sudo apt install vim flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.visualstudio.code
