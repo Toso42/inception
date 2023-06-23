cec6c3cdd8966355d558522318f39056b012d5a6
Toso <tcorax4@gmail.com>
2023-06-23 17:15:02 +0200

    bon

--- vmconfig.sh
+++ vmconfig.sh
@@ -1,2 +1,9 @@
+# manually:
+# su -
+# adduser *user sudo
+# logout and login
+# sudo apt install git
+# git clone git@github.com:Toso42/inception.git
+
 sudo apt update && sudo apt upgrade -y \
 sudo apt install git
