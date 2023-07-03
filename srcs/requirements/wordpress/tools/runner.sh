25d5e49f6140447bb1e1de9b634034d21108c474
Toso <tcorax42@gmail.com>
2023-07-03 08:58:53 +0200

    maria db start

--- srcs/requirements/wordpress/tools/runner.sh
+++ srcs/requirements/wordpress/tools/runner.sh
@@ -5,7 +5,12 @@ echo "hello from wordpress script!"
 # cat /var/www/html/test.html
 
 # wordpress install and configuration
-wp core download
-ls
+CONF=/var/www/html/wp-config.php
+if [ -f "$CONF" ]; then
+	echo "wordpress already configured"
+else
+	cd /var/www/html/
+	wp core download --allow-root
+fi
 
 while true; do sleep 1; done
