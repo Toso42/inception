bd748521e5d5c07c850142d98e239d9dded058a5
Toso <tcorax42@gmail.com>
2023-07-05 20:12:24 +0200

    mysql up
    managed to run mysql

--- srcs/requirements/mariadb/tools/runner.sh
+++ srcs/requirements/mariadb/tools/runner.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
 
-while true; do sleep 1; done
+service mysql start
+
+# wait a few seconds to get service up before configuring db
+sleep 2
+
+echo "Configuring Database"
+echo "CREATE DATABASE IF NOT EXISTS $WPDB_NAME ;" > db.conf
+echo "CREATE USER IF NOT EXISTS '$WPDB_USER'@'%' IDENTIFIED BY '$WPDB_PASSWORD' ;" >> db.conf
+echo "GRANT ALL PRIVILEGES ON $WPDB_NAME.* TO '$WPDB_USER'@'%' ;" >> db.conf
+echo "FLUSH PRIVILEGES ;" >> db.conf
+
+mysql < db.conf
+service mysql stop
+
+#while true; do sleep 1; done
+
+# mysql daemon run
+echo "Restarting Mysql"
+mysqld
--- srcs/requirements/wordpress/tools/runner.sh
+++ srcs/requirements/wordpress/tools/runner.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 echo "hello from wordpress script!"
-# cat /var/www/html/test.php
-# cat /var/www/html/test.html
 
-# wordpress install and configuration
 CONF=/var/www/html/wp-config.php
 if [ -f "$CONF" ]; then
 	echo "wordpress already configured"
